242d03bb4ddee5a59be9d7984a7b3ddfe508fbaa
zhanghongan <zhanghongan2021@163.com>
2021-12-06 18:39:02 +0800

    官方文档

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,6 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-
-git add -A
-git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
-git push -f https://github.com/Irene200825/Irene200825.github.io.git main
-
-
-
 # 生成静态文件
 npm run docs:build
 
@@ -20,7 +11,7 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m '优化'
 
 # 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
 git push -f https://github.com/Irene200825/Irene200825.github.io.git main
