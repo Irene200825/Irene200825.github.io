81342862f951946981a7d0a53d50090e2ee4d1e1
zhanghongan <zhanghongan2021@163.com>
2021-12-06 16:21:01 +0800

    初始化

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,6 +5,7 @@ export default defineUserConfig<DefaultThemeOptions>({
   lang: 'en-US',
   title: '安子的博客',
   description: 'Just playing around',
+  base: '/notes/'
 
   // themeConfig: {
   //   logo: 'https://vuejs.org/images/logo.png',
